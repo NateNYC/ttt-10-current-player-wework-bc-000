fde0f98c9c0cd371ad73c36ce7b1a713d27f4e06
Nathan de Paz <nathan.depaz@wework.com>
2018-02-19 21:05:23 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,7 +6,7 @@ end
 # this method will call turn_count(board) as part of its logic
 
 def current_player(board)
-  if turn_count(board).to_i == 
+  if turn_count(board).to_i == turn_count(board).even?
   
  
 end
