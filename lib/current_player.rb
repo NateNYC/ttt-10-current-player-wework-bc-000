ea04240e546e4a700b63d13984d4a51330449f54
Nathan de Paz <nathan.depaz@wework.com>
2018-02-19 21:19:03 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,7 +8,7 @@ end
 # this method will call turn_count(board) as part of its logic
 
 def current_player(board)
-  if turn_count(board).to_i == turn_count(board).even?
+  if turn_count(board).to_i % 2
     current_player = "O"
   elsif
     current_player = "X"
