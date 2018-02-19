0f12949eb47bd6236b3ba53def42aecf17798fce
Nathan de Paz <nathan.depaz@wework.com>
2018-02-19 21:14:05 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,15 @@
 def turn_count(board)
- board.count{|token| token == "X" || token == "O"}
+ counter = 0
+ while counter > 9
+  board.count{|token| token == "X" || token == "O"}
+  counter += 1
 end
 
 
 # this method will call turn_count(board) as part of its logic
 
 def current_player(board)
+  
   if turn_count(board).to_i == turn_count(board).even?
     current_player = "O"
   elsif
